ec26b1b3db9f342a486af3b6c5f34c21b2d3a17f
A Nuri <ahmettonuri@gmail.com>
2024-10-12 19:16:49 +0300

    aa

---
+++ day01/Operators.js
@@ -0,0 +1,17 @@
+console.log(10 + 5);
+console.log(20 - 5);
+console.log(20 * 2);
+console.log(10 / 4);
+console.log("---------");
+
+let a = 20;
+a = 40;
+console.log(a);
+a += 40;
+console.log(a);
+
+console.log(10 > 5);
+console.log(10 >= 10);
+
+console.log(100 == 100);
+console.log(100 == "100");
---
+++ day01/Variables.js
@@ -0,0 +1,33 @@
+console.log("Hello World");
+console.log('Hello Cydeo')
+
+console.log("--------------")
+
+let schoolName = "Aliminyum İlkokulu"
+console.log(schoolName)
+
+if(5>4) {
+    let a = 100
+}
+//console.log(a)
+
+const number_of_hours_in_a_day = 24;
+//number_of_hours_in_a_day = 25; const can not be changed
+
+console.log(number_of_hours_in_a_day)
+
+console.log("---------")
+
+age = 28
+
+console.log(age)
+
+let score = 98;
+console.log(score)
+
+let gpa = 3.5;
+console.log(gpa)
+
+console.log(typeof gpa)
+console.log(typeof score)
+console.log(typeof schoolName)
